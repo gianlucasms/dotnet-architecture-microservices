d02cea9d6ad6868fbb85b077c58842c9309ca026
Gian <gian.silva@sixconsult.com.br>
2022-11-11 12:14:32 -0300

    first structure

---
+++ S07.ErudioMicroservices.NET6-FirstMicrosservices/GeekShopping/GeekShopping.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeekShopping.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
